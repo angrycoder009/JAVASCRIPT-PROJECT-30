e8cd43aa271f966c5d136f9595f125baaf3ac3a7
Gopal yadav <yadavgopal6675@gmail.com>
2024-06-09 22:57:00 +0530

    start variable topic

---
+++ JS/js-basic/variables/script.js
@@ -0,0 +1,14 @@
+/*variable in javascript */
+// let username = 'gopal yadav'
+// let userIntro = 'my name is Gopal Yadav'
+let firstName = 'gopal'
+let lastName = 'yadav'
+let age = 21
+let isHappy = true
+let a;
+// let userIntro = 'Hi , I am ' + username
+let userIntro = 
+        'Hi I am '
+        +firstName + " " 
+        + lastName 
+        +'.I am '+ age + '  year old.'

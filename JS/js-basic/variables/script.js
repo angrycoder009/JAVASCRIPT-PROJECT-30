cc64d1a70a54a29fdba0ba14f672b5237c6e774c
Gopal yadav <yadavgopal6675@gmail.com>
2024-06-10 16:29:29 +0530

    learn variable topic

--- JS/js-basic/variables/script.js
+++ JS/js-basic/variables/script.js
@@ -1,14 +1,22 @@
 /*variable in javascript */
 // let username = 'gopal yadav'
 // let userIntro = 'my name is Gopal Yadav'
-let firstName = 'gopal'
-let lastName = 'yadav'
+{var firstName = 'gopal'}
+//var is not block scopt it can be acces anywehere
+{let lastName = 'yadav'}
+//let is limite to block scope one defined in under bracket it never be 
+//used anywhere only limited to block scope
 let age = 21
 let isHappy = true
 let a;
+ a ='david'
+
+ const hoursInDay = 24
+
 // let userIntro = 'Hi , I am ' + username
+firstName = 100
 let userIntro = 
         'Hi I am '
-        +firstName + " " 
-        + lastName 
+        +a + " " 
+        + lastName   //here we getting error because let is limited to only scope
         +'.I am '+ age + '  year old.'

065d52ab5a592c5dde9e3293ac141a8315a50c42
Gopal yadav <yadavgopal6675@gmail.com>
2024-06-10 17:15:01 +0530

    used bebuggin tool

---
+++ JS/js-basic/variables/app.js
@@ -0,0 +1,6 @@
+debugger
+console.log(firstName)
+var firstName= 'Gopal '
+let lastName = 'yadav'
+let age = 21
+let userIntro = 'My name is ' + firstName + ' '+lastName + 'and I am ' + age + ' years old' 

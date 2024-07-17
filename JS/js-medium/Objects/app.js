547936975752358611b0220b2a0ebe9c76189fe5
Gopal yadav <yadavgopal6675@gmail.com>
2024-07-17 20:42:07 +0530

    object starts

---
+++ JS/js-medium/Objects/app.js
@@ -0,0 +1,9 @@
+const user = {firstName : 'Akash',
+              lastName : 'Singh',
+              age : 26,
+              education : 12,
+              city : 'Banglore'
+}
+console.log(user)
+// console.log(typeof user)
+

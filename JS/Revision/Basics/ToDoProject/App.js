e473675309538b076ab4a422381ebd7bb2aaacf2
Gopal yadav <yadavgopal6675@gmail.com>
2024-09-04 02:28:40 +0530

    displays todo using loops and dom

--- JS/Revision/Basics/ToDoProject/App.js
+++ JS/Revision/Basics/ToDoProject/App.js
@@ -1,9 +1,21 @@
 const myArray =[];
-
+renderToDoList();
+function renderToDoList(){
+let todolistHtml=''
+for(let i = 0 ; i <myArray.length;i++){
+    const todoList =myArray[i];
+    const html = `<p>${todoList}</p>`
+    todolistHtml +=html
+}
+console.log(todolistHtml)
+document.querySelector('.js-todo-lists')
+    .innerHTML =todolistHtml;
+}
 function AddTodo(){
     const input = document.querySelector('.value')
     const retriveValue =input.value
     myArray.push(retriveValue)
     console.log(myArray)
     input.value = ''
+    renderToDoList();
 }

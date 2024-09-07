dcb26eb3dc7f86ca485471e4c5dc21089895d06f
Gopal yadav <yadavgopal6675@gmail.com>
2024-09-08 04:03:57 +0530

    complet project todo list

--- JS/Revision/Basics/ToDoProject/App.js
+++ JS/Revision/Basics/ToDoProject/App.js
@@ -1,21 +1,42 @@
-const myArray =[];
+const myArray = [{
+    name : 'make dinner',
+     date : '2023-12-9'
+}];
 renderToDoList();
-function renderToDoList(){
-let todolistHtml=''
-for(let i = 0 ; i <myArray.length;i++){
-    const todoList =myArray[i];
-    const html = `<p>${todoList}</p>`
-    todolistHtml +=html
+
+function renderToDoList() {
+    let todolistHtml = '';
+    for (let i = 0; i < myArray.length; i++) {
+        const todoObj = myArray[i];
+        // const name = todoObj.name
+        // const date = todoObj.dueDate
+        const {name,date}=todoObj;
+        const html = `
+       <div> ${name}</div>
+        <div> ${date}</div>
+        <button onclick="
+        myArray.splice(${i}, 1);
+        renderToDoList();
+        ">Delete</button>
+       
+        `;
+        todolistHtml += html;
+    }
+    console.log(todolistHtml);
+    document.querySelector('.js-todo-lists')
+        .innerHTML = todolistHtml;
 }
-console.log(todolistHtml)
-document.querySelector('.js-todo-lists')
-    .innerHTML =todolistHtml;
-}
-function AddTodo(){
-    const input = document.querySelector('.value')
-    const retriveValue =input.value
-    myArray.push(retriveValue)
-    console.log(myArray)
-    input.value = ''
+
+function AddTodo() {
+    const input = document.querySelector('.value');
+    const date = document.querySelector('.js-date-input')
+    const dueDate = date.value;
+    const retriveValue = input.value;
+    myArray.push({
+        name :retriveValue ,
+        date :dueDate
+    })
+    console.log(myArray);
+    input.value = '';
     renderToDoList();
 }

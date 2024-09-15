4e6a193f18c7c213944bf5e61b2d5dc7a6f733e3
Gopal yadav <yadavgopal6675@gmail.com>
2024-09-15 19:12:53 +0530

    add feat play with keyboard using onkeydown prop

--- JS/Revision/Basics/rock-paper-scissor-advanced-function/rock-paper-scissor-event.js
+++ JS/Revision/Basics/rock-paper-scissor-advanced-function/rock-paper-scissor-event.js
@@ -44,6 +44,16 @@ let score = JSON.parse(localStorage.getItem('score')) || {
     playGame('scissors');
   });
 
+  document.body.addEventListener('keydown',(event)=>{
+    if(event.key ==='r'){
+      playGame('rock')
+    } else if(event.key ==='p'){
+      playGame('paper')
+    } else if(event.key==='s'){
+      playGame('scissors')
+    }
+  });
+
   function playGame(playerMove) {
     const computerMove = pickComputerMove();
   

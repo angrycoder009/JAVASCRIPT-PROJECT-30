7ca5d33ee9f2cb0f81adfec8defc94f083a547d8
Gopal yadav <yadavgopal6675@gmail.com>
2024-10-19 15:33:11 +0530

    generate the html in cart summaray

--- javascript-amazon-project/javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/amazon.js
@@ -1,6 +1,6 @@
 import {cart,addToCart,updateCartQuantity,showCartAdded} from '../data/cart.js';
 import { products } from '../data/products.js';
-
+ 
 //creating a varaible to store the product data
 let storeHtml = ''
 //loop throght daata product to put in each elemnt of html
